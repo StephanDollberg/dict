22ca53b64c9a9e7e4ffb6a19a80c1d327a8bae1f
Stephan Dollberg <stephan.dollberg@gmail.com>
2017-09-03 21:34:01 +0100

    updated bench_branch.sh script to take branches as arguments

--- perf/bench_branch.sh
+++ perf/bench_branch.sh
@@ -2,8 +2,9 @@
 
 set -x
 
-branch=$(git rev-parse --abbrev-ref HEAD)
-
-make clean ; make;
-
-./test_main --benchmark_repetitions=3 --benchmark_report_aggregates_only=true --benchmark_out=$branch.json --benchmark_out_format=json --benchmark_filter=dict_
+for branch in "$@"
+do
+    git co $branch
+    make clean ; make;
+    ./test_main --benchmark_repetitions=3 --benchmark_report_aggregates_only=true --benchmark_out=$branch.json --benchmark_out_format=json --benchmark_filter=dict_
+done
