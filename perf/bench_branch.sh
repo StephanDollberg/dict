b08c5acd2ff9554a7fc402645c923b73d9020402
Stephan Dollberg <stephan.dollberg@gmail.com>
2018-01-20 19:34:54 +0000

    reformat bench_branch.sh

--- perf/bench_branch.sh
+++ perf/bench_branch.sh
@@ -6,5 +6,9 @@ for branch in "$@"
 do
     git co $branch
     make clean ; make;
-    ./test_main --benchmark_repetitions=3 --benchmark_report_aggregates_only=true --benchmark_out=$branch.json --benchmark_out_format=json --benchmark_filter=dict_
+    ./test_main --benchmark_repetitions=3 \
+    			--benchmark_report_aggregates_only=true \
+    			--benchmark_out=$branch.json \
+    			--benchmark_out_format=json \
+    			--benchmark_filter=dict_
 done
